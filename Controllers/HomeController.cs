8a96d5b43bd939c0bdca41222c6edf507905e395
Tyler <tylerelijahsmith15@gmail.com>
2021-02-10 23:58:02 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TopFive.Models;
+
+namespace TopFive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> restaurantList = new List<string>();
+
+            foreach (Restaurant r in Restaurant.GetRestaurants())
+            {
+                if (r.FavoriteDish == null)
+                { 
+                    r.FavoriteDish = "It's all good!";
+                }
+                string phone = r.PhoneNumber.ToString();
+                string area = phone.Substring(0, 3);
+                string major = phone.Substring(3, 3);
+                string minor = phone.Substring(6);
+                string formatted = string.Format("{0}-{1}-{2}", area, major, minor);
+                r.PhoneNumber = formatted;
+                restaurantList.Add($"#{r.RestaurantRanking}, Restaurant Name: {r.RestaurantName}, Best Dish: {r.FavoriteDish}, Address: {r.Address}, Phone: {r.PhoneNumber}, Website: {r.Website}");
+            }
+
+            return View(restaurantList);
+        }
+
+        public IActionResult OtherList()
+        {
+            return View(RestaurantStorage.Restaurants);
+        }
+
+        [HttpGet]
+        public IActionResult NewSubmit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewSubmit(OtherRestaurant newRestuarant)
+        {
+            RestaurantStorage.AddNewRestaurant(newRestuarant);
+            return View("OtherList");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopFive.Models
+{
+    public class Restaurant
+    {
+        public Restaurant(int rank)
+        {
+            RestaurantRanking = rank;
+        }
+        public int RestaurantRanking { get; }
+        public string RestaurantName { get; set; }
+        public string FavoriteDish { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Website { get; set; } = "Coming Soon";
+
+        public static Restaurant[] GetRestaurants()
+        {
+            Restaurant r1 = new Restaurant(1)
+            {
+                RestaurantName = "Bombay House",
+                FavoriteDish = null,
+                Address = "463 N University Avenue",
+                PhoneNumber = "8013736677",
+                Website = "www.bombayhouse.com"
+            };
+
+            Restaurant r2 = new Restaurant(2)
+            {
+                RestaurantName = "Bam Bams BBQ",
+                FavoriteDish = "Brisket or Ribs",
+                Address = "1708 South State Street",
+                PhoneNumber = "8012251324",
+                Website = "www.bambamsbbq.com"
+            };
+
+            Restaurant r3 = new Restaurant(3)
+            {
+                RestaurantName = "Don Joaquin",
+                FavoriteDish = "Carne Asada Burrito",
+                Address = "150 W 1230 N St",
+                PhoneNumber = "8014002894",
+                Website = "www.donjoaquinsttacosprovo.com"
+            };
+
+            Restaurant r4 = new Restaurant(4)
+            {
+                RestaurantName = "Ruby River Steakhouse",
+                FavoriteDish = "Anything with Steak",
+                Address = "1454 S. University Ave.",
+                PhoneNumber = "8013710648",
+                Website = "www.rubyriver.com"
+            };
+
+            Restaurant r5 = new Restaurant(5)
+            {
+                RestaurantName = "Red Fuego",
+                FavoriteDish = "Lomo Saltado",
+                Address = "820 E 800 N",
+                PhoneNumber = "8019609095"
+            };
+
+            return new Restaurant[] { r1, r2, r3, r4, r5 };
+        }
+    }
+
+    public class OtherRestaurant
+    {
+        public string Name { get; set; }
+        public string RestaurantName { get; set; }
+        public string FavoriteDish { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+
+    public static class RestaurantStorage
+    {
+        private static List<OtherRestaurant> restaurants = new List<OtherRestaurant>();
+
+        public static IEnumerable<OtherRestaurant> Restaurants => restaurants;
+
+        public static void AddNewRestaurant(OtherRestaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+        }
+    }
+}
